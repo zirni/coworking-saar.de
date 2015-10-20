4751d49f4b98d3fed591b329a763ec8bdff16880
Matthias Zirnstein <matthias.zirnstein@googlemail.com>
2015-10-20 20:32:27 +0200

    initial

---
+++ hakunamata.rb
@@ -0,0 +1,17 @@
+require 'haml'
+require 'yaml'
+require 'ostruct'
+
+template = File.read('layout.haml')
+
+config = YAML.load_file('config.yml')
+
+config = config.map {|c| OpenStruct.new(c)}.group_by(&:city)
+
+style = File.read('screen.css')
+
+haml_engine = Haml::Engine.new(template)
+
+output = haml_engine.render(Object.new, config: config, style: style)
+
+File.open('index.html', 'w+') {|f| f.write(output) }
